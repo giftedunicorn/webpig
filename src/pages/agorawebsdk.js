02f7160170c57a83e2c529b87842ba959ccd263d
ffnew <cloudmass9@gmail.com>
2023-08-01 00:10:40 +0800

    Fix import agora sdk, not import from server side

--- src/pages/agorawebsdk.js
+++ src/pages/agorawebsdk.js
@@ -3,29 +3,33 @@ import {useState, useEffect} from 'react';
 import {
     Button, Typography, Input,
 } from 'antd';
+import * as log from 'loglevel';
 import styles from '@/styles/App.module.css'
-const AgoraWebRtc = require('../agorawebsdk');
+// import dynamic from "next/dynamic";
+// const AgoraWebSdk = dynamic(() => import('../agorawebsdk'), { ssr: false });
 
 const {Title, Paragraph} = Typography;
+
+// console.log(AgoraWebSdk)
 
 function App() {
     const [appId, setAppId] = useState('');
     const [channelName, setChannelName] = useState('');
     const [userId, setUserId] = useState();
+    const [token, setToken] = useState('');
 
-    useEffect(() => {
-    }, []);
-
-    const joinOnClick = () => {
-        AgoraWebRtc.join()
+    const joinOnClick = async () => {
+        const { join } = await import('../agorawebsdk')
+        join(appId, channelName, null, null)
             .then(() => {
             }).catch((err) => {
                 log.error(err);
             });
     };
 
-    const leaveOnClick = () => {
-        AgoraWebRtc.leave();
+    const leaveOnClick = async () => {
+        const { leave } = await import('../agorawebsdk')
+        leave()
     };
 
     const renderHelper = () => {
